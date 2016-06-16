0d5ab405cee01f2b7f4fb874dca2aa5403fc1d40
nbkhope <ryumemaru@yahoo.com>
2016-06-16 16:36:14 -0700

    Add image under Welcome text

--- MyProject/index.ios.js
+++ MyProject/index.ios.js
@@ -9,7 +9,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 
 class MyProject extends Component {
@@ -19,13 +20,8 @@ class MyProject extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <Image source={require('./images/dbc.jpg')} />
       </View>
     );
   }
