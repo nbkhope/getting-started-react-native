eb874e1206975bf2d5de6e60bb1b7ea9c467c12a
nbkhope <ryumemaru@yahoo.com>
2016-06-16 16:23:41 -0700

    Change container background color to antiquewhite

--- MyProject/index.ios.js
+++ MyProject/index.ios.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'antiquewhite',
   },
   welcome: {
     fontSize: 20,
